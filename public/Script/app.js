63f8c0a69f1c9fd9bca1c02096cbc91b1c66af05
Mike Pham <mikephse@gmail.com>
2020-10-06 04:39:45 -0400

    initial commit

---
+++ public/Script/app.js
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expression
+/* * Author: Le Minh Pham
+ * StudentID: 300814556
+ * Date: Oct 6th 2020 
+ */
+(function () {
+    function Start() {
+        console.log("App Started...");
+    }
+    window.addEventListener("load", Start);
+})();
