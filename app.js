637a85c99b3a380b5bbff6c72a26c002c0b80461
Mike Pham <mikephse@gmail.com>
2020-10-22 02:44:30 -0400

    revert to previous version

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
- /* Author: Le Minh Pham
+/* Author: Le Minh Pham
  * StudentID: 300814556
- * Date: Oct 6th 2020 
+ * Date: Oct 6th 2020
  */
 
- // installed 3rd party packages
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+// installed 3rd party packages
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-const { default: sslRedirect } = require('heroku-ssl-redirect');
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+const { default: sslRedirect } = require("heroku-ssl-redirect");
 
 let app = express();
 
@@ -20,33 +20,33 @@ let app = express();
 app.use(sslRedirect());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');//express -e
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); //express -e
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: 'Error'});
+  res.render("error", { title: "Error" });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
- /* Author: Le Minh Pham
+/* Author: Le Minh Pham
  * StudentID: 300814556
- * Date: Oct 6th 2020 
+ * Date: Oct 6th 2020
  */
 
 /**
  * Module dependencies.
  */
 
-var app = require('./app');
-var debug = require('debug')('express-porfolio:server');
-var http = require('http');
+var app = require("./config/app");
+var debug = require("debug")("express-porfolio:server");
+var http = require("http");
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -31,8 +31,8 @@ var server = http.createServer(app);
  */
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -59,22 +59,20 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -88,8 +86,6 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
